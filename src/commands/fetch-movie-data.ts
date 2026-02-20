8370aea358f615cafb7d2a1f98d4f389fe636110
Moray Macdonald <moray.macdonald1@gmail.com>
2026-02-20 19:57:39 +0000

    feat: Add simple command to pull data into a note

---
+++ src/commands/fetch-movie-data.ts
@@ -0,0 +1,39 @@
+import {Editor, MarkdownView, Notice} from 'obsidian';
+import type OmdbFetcher from '../main';
+import {fetchByTitle, OmdbError} from '../utils/api';
+
+const FILENAME_PATTERN = /^(.+)\s+\((\d{4})\)$/;
+
+export async function fetchMovieData(editor: Editor, view: MarkdownView, plugin: OmdbFetcher): Promise<void> {
+	const file = view.file;
+	if (!file) {
+		new Notice('No active file');
+		return;
+	}
+
+	const match = file.basename.match(FILENAME_PATTERN);
+	if (!match) {
+		// eslint-disable-next-line obsidianmd/ui/sentence-case
+		new Notice('Filename must be in the format: Name (Year)');
+		return;
+	}
+
+	const title = match[1] as string;
+	const year = parseInt(match[2] as string, 10);
+
+	try {
+		const result = await fetchByTitle(plugin.settings.omdbApiKey, title, {year});
+		const json = JSON.stringify(result, null, 2);
+		const lastLine = editor.lastLine();
+		const lastLineContent = editor.getLine(lastLine);
+		const prefix = lastLineContent.length > 0 ? '\n' : '';
+		editor.replaceRange(
+			`${prefix}\`\`\`json\n${json}\n\`\`\`\n`,
+			{line: lastLine, ch: lastLineContent.length},
+		);
+		new Notice(`Fetched data for "${result.Title}"`);
+	} catch (err) {
+		const msg = err instanceof OmdbError ? err.message : 'Unknown error fetching movie data';
+		new Notice(msg);
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, OmdbFetcherSettings, OmdbFetcherSettingTab} from "./settings";
-
-// Remember to rename these classes and interfaces!
+import {MarkdownView, Plugin} from 'obsidian';
+import {DEFAULT_SETTINGS, OmdbFetcherSettings, OmdbFetcherSettingTab} from './settings';
+import {fetchMovieData} from './commands/fetch-movie-data';
 
 export default class OmdbFetcher extends Plugin {
 	settings: OmdbFetcherSettings;
@@ -9,68 +8,20 @@ export default class OmdbFetcher extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new OmdbFetcherModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new OmdbFetcherModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'fetch-movie-data',
+			name: 'Fetch movie data',
+			editorCheckCallback: (checking, editor) => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (!view) return false;
+				if (!checking) {
+					void fetchMovieData(editor, view, this);
 				}
-				return false;
-			}
+				return true;
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new OmdbFetcherSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		
-
-	}
-
-	onunload() {
 	}
 
 	async loadSettings() {
@@ -79,21 +30,5 @@ export default class OmdbFetcher extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class OmdbFetcherModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
