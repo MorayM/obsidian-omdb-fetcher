33d0bd15c4c2787910201c07908411f41e90adff
Moray Macdonald <moray.macdonald1@gmail.com>
2026-02-20 09:04:57 +0000

    chore: Rename components

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import OmdbFetcher from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface OmdbFetcherSettings {
+	omdbApiKey: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export const DEFAULT_SETTINGS: OmdbFetcherSettings = {
+	omdbApiKey: 'default'
 }
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class OmdbFetcherSettingTab extends PluginSettingTab {
+	plugin: OmdbFetcher;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: OmdbFetcher) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -23,13 +23,16 @@ export class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
+			// OMDb is not valid sentance case
+			// eslint-disable-next-line obsidianmd/ui/sentence-case
+			.setName('OMDb API key')
+			// eslint-disable-next-line obsidianmd/ui/sentence-case
+			.setDesc('Enter your OMDb API key')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.omdbApiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.omdbApiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
